0333203f60b2267dabcc4366b806b1f10dd5a37f
Zeroo Infinite <zeroo-infinite@outlook.com>
2020-03-08 09:07:09 +1100

    add custome editor helper

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -1,7 +1,7 @@
 import React, { useMemo, useState, useCallback } from "react";
-import { createEditor, Editor, Transforms, Text } from 'slate'
+import { createEditor } from 'slate'
 import { Slate, Editable, withReact  } from 'slate-react'
-
+import CustomEditor from '../scripts/EditorHelper'
 
 const TextEditor = () => {
   const editor = useMemo(() => withReact(createEditor()), []);
@@ -51,27 +51,14 @@ const TextEditor = () => {
             // When "`" is pressed, keep our existing code block logic.
             case '`': {
               event.preventDefault()
-              const [match] = Editor.nodes(editor, {
-                match: n => n.type === 'code',
-              })
-              Transforms.setNodes(
-                editor,
-                { type: match ? 'paragraph' : 'code' },
-                { match: n => Editor.isBlock(editor, n) }
-              )
+              CustomEditor.toggleCodeBlock(editor)
               break
             }
 
             // When "B" is pressed, bold the text in the selection.
             case 'b': {
               event.preventDefault()
-              Transforms.setNodes(
-                editor,
-                { bold: true },
-                // Apply it to text nodes, and split the text node up if the
-                // selection is overlapping only part of it.
-                { match: n => Text.isText(n), split: true }
-              )
+              CustomEditor.toggleBoldMark(editor)
               break
             }
 
