05deb501340f9cfe6277ce8d34b16b235a9de30d
Zeroo Infinite <zeroo-infinite@outlook.com>
2020-03-08 09:02:10 +1100

    Adding custom formatting

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -1,27 +1,112 @@
-import React, { useEffect, useMemo, useState } from "react";
-import { createEditor } from 'slate'
+import React, { useMemo, useState, useCallback } from "react";
+import { createEditor, Editor, Transforms, Text } from 'slate'
 import { Slate, Editable, withReact  } from 'slate-react'
 
 
-const initialValue = [
-  {
-    children: [
-      { text: 'This is editable plain text, just like a <textarea>!' },
-    ],
-  },
-];
-
 const TextEditor = () => {
   const editor = useMemo(() => withReact(createEditor()), []);
+  const initialValue = [
+    {
+      children: [
+        { text: 'This is editable plain text, just like a <textarea>!' },
+      ],
+    },
+  ];
   const [value, setValue] = useState(initialValue);
 
+    // Define a rendering function based on the element passed to `props`. We use
+  // `useCallback` here to memoize the function for subsequent renders.
+  const renderElement = useCallback(props => {
+    switch (props.element.type) {
+      case 'code':
+        return <CodeElement {...props} />
+      default:
+        return <DefaultElement {...props} />
+    }
+  }, []);
+
+  // Define a leaf rendering function that is memoized with `useCallback`.
+  const renderLeaf = useCallback(props => {
+    return <Leaf {...props} />
+  }, [])
 
   return (
     <Slate editor={editor} value={value} onChange={value => setValue(value)}>
-      <Editable />
+      <Editable
+        renderElement={renderElement}
+        renderLeaf={renderLeaf}
+        onKeyDown={event => {
+          if (event.key === '&') {
+            // Prevent the ampersand character from being inserted.
+            event.preventDefault()
+            // Execute the `insertText` method when the event occurs.
+            editor.insertText("and")
+          }
+
+          if (!event.ctrlKey) {
+            return
+          }
+
+          switch (event.key) {
+            // When "`" is pressed, keep our existing code block logic.
+            case '`': {
+              event.preventDefault()
+              const [match] = Editor.nodes(editor, {
+                match: n => n.type === 'code',
+              })
+              Transforms.setNodes(
+                editor,
+                { type: match ? 'paragraph' : 'code' },
+                { match: n => Editor.isBlock(editor, n) }
+              )
+              break
+            }
+
+            // When "B" is pressed, bold the text in the selection.
+            case 'b': {
+              event.preventDefault()
+              Transforms.setNodes(
+                editor,
+                { bold: true },
+                // Apply it to text nodes, and split the text node up if the
+                // selection is overlapping only part of it.
+                { match: n => Text.isText(n), split: true }
+              )
+              break
+            }
+
+            default: {
+              return 
+            }
+          }
+        }}
+      />
     </Slate>
   )
   
 };
+
+const CodeElement = props => {
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  )
+}
+
+const DefaultElement = props => {
+  return <p {...props.attributes}>{props.children}</p>
+}
+
+const Leaf = props => {
+  return (
+    <span
+      {...props.attributes}
+      style={{ fontWeight: props.leaf.bold ? 'bold' : 'normal' }}
+    >
+      {props.children}
+    </span>
+  )
+}
 
 export default TextEditor;
