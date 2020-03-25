cc5426a5d77e78d1febe52db14fb3cc168fc3469
Zeroo Infinite <zeroo-infinite@outlook.com>
2020-03-26 08:12:31 +1100

    Create toolbar menu

---
+++ src/components/toolbarMenu.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { useSlate } from 'slate-react'
+
+import { Button, Icon, Toolbar } from './editorComponent'
+import { toggleBlock, toggleMark, isBlockActive, isMarkActive } from '../scripts/EditorHelper'
+
+
+export const ToolbarMenu = () => {
+  return (
+    <Toolbar>
+      <MarkButton format="bold" icon="format_bold" />
+      <MarkButton format="italic" icon="format_italic" />
+      <MarkButton format="underline" icon="format_underlined" />
+      <MarkButton format="code" icon="code" />
+      <BlockButton format="heading-one" icon="looks_one" />
+      <BlockButton format="heading-two" icon="looks_two" />
+      <BlockButton format="block-quote" icon="format_quote" />
+      <BlockButton format="numbered-list" icon="format_list_numbered" />
+      <BlockButton format="bulleted-list" icon="format_list_bulleted" />
+    </Toolbar>
+  )
+}
+
+const BlockButton = ({ format, icon }) => {
+  const editor = useSlate()
+  return (
+    <Button
+      active={isBlockActive(editor, format)}
+      onMouseDown={event => {
+        event.preventDefault()
+        toggleBlock(editor, format)
+      }}
+    >
+      <Icon>{icon}</Icon>
+    </Button>
+  )
+}
+
+const MarkButton = ({ format, icon }) => {
+  const editor = useSlate()
+  return (
+    <Button
+      active={isMarkActive(editor, format)}
+      onMouseDown={event => {
+        event.preventDefault()
+        toggleMark(editor, format)
+      }}
+    >
+      <Icon>{icon}</Icon>
+    </Button>
+  )
+}
